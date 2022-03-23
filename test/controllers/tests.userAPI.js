3a6318085d177c10f2b315bcd1f45fa020bc766b
Juan <Juanjspm@hotmail.com>
2022-03-23 20:26:57 +0000

    RED: Controllers / User / API Should delete user info:

--- test/controllers/tests.userAPI.js
+++ test/controllers/tests.userAPI.js
@@ -40,5 +40,23 @@ describe ("Controllers / User / API", function()
         
         assert.equal(results[0].NAME, "nombre1");
         assert.equal(results[0].PASSWD, password);
-    })
+    });
+
+    it("Should delete user info", async() =>
+    {
+        const password = md5("1234");
+        await testSetupContents().givenAnyUser({name: "nombre1", surname: "apellidos1", display_name: "display1", email: "email1@gmail.com", phone: "123 456 789", passwd: password, rol: 0, points: 5, organization: "ULPGC", time_stamp: "17/03/2022 12:13:25"});
+        await testSetupContents().givenAnyUser({name: "nombre1", surname: "apellidos1", display_name: "display2", email: "email2@gmail.com", phone: "123 456 789", passwd: password, rol: 0, points: 5, organization: "ULPGC", time_stamp: "17/03/2022 12:13:25"});
+        let anyUserId = {
+            ID: 1
+        };
+        let results = await testSetupContents().getAnyUser({name: "nombre1", surname: "apellidos1"});
+        assert.equal(results[0].NAME, "nombre1");
+        assert.equal(results[0].PASSWD, password);
+        assert.equal(results.length, 2);
+
+        await appAgent.delete("/api/users").send(anyUserId);
+        results = await testSetupContents().getAnyUser({name: "nombre1", surname: "apellidos1"});
+        assert.equal(results.length, 1);
+    });
 });
