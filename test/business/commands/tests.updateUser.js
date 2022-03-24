1893b6349307574bd5fb1ded3d7dbb4fb1d436cd
Ione <ione.sanabria101@alu.ulpgc.es>
2022-03-24 19:04:55 +0000

    Red: Business / Command / User Should update user info (fixed assert calls)

--- test/business/commands/tests.updateUser.js
+++ test/business/commands/tests.updateUser.js
@@ -24,23 +24,29 @@ describe ("Business / Command / User", function()
         await testSetupContents().givenAnyUser({name: "nombre1", surname: "apellidos1", display_name: "display1", email: "email1@gmail.com", phone: "123 456 789", passwd: password, rol: 0, points: 5, organization: "ULPGC", time_stamp: "17/03/2022 12:13:25"});
         await testSetupContents().givenAnyUser({name: "nombre2", surname: "apellidos2", display_name: "display2", email: "email2@gmail.com", phone: "123 456 789", passwd: password, rol: 0, points: 5, organization: "ULPGC", time_stamp: "17/03/2022 12:13:25"});
         let updateData = {
+            id: 1,
             name: "nombre3",
             surname: "apellidos3",
             display_name: "display3",
-            phone: "987 654 321",
+            email: "email1@gmail.com",
+            phone: '987654321',
             passwd: password2,
-            organization: "ULL"
+            rol: 1,
+            points: 47,
+            organization: "ULL",
         };
-        
+
         await commandFactory.updateUser().execute(updateData);
         let results = await testSetupContents().getAnyUser({name: "nombre3", surname: "apellidos3"});
-
-        assert.equal(results.name, "nombre3");
-        assert.equal(results.surname,"apellidos3");
-        assert.equal(results.display_name, "display3");
-        assert.equal(results.phone, "987 654 321");
-        assert.equal(results.passwd, password2);
-        assert.equal(results.organization, "ULL");
+        assert.equal(results[0].NAME, "nombre3");
+        assert.equal(results[0].SURNAME, "apellidos3");
+        assert.equal(results[0].DISPLAY_NAME, "display3");
+        assert.equal(results[0].EMAIL, "email1@gmail.com");
+        assert.equal(results[0].PHONE, '987654321');
+        assert.equal(results[0].PASSWD, password2);
+        assert.equal(results[0].ROL, 1);
+        assert.equal(results[0].POINTS, 47);
+        assert.equal(results[0].ORGANIZATION, "ULL");
 
     });
 
