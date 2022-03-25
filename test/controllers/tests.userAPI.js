14dc20171a0d7887b73a975dd879ba1bbb763ff7
Ione <ione.sanabria101@alu.ulpgc.es>
2022-03-25 18:20:40 +0000

    Red: Controllers / User / API Should update user info:

--- test/controllers/tests.userAPI.js
+++ test/controllers/tests.userAPI.js
@@ -75,4 +75,36 @@ describe ("Controllers / User / API", function()
         results = await testSetupContents().getAnyUser({name: "nombre1", surname: "apellidos1"});
         assert.equal(results.length, 1);
     });
+
+    it("Should update user info", async() =>
+    {
+        const password = md5("1234");
+        const password2 = md5("4321");
+        await testSetupContents().givenAnyUser({name: "nombre1", surname: "apellidos1", display_name: "display1", email: "email1@gmail.com", phone: "123 456 789", passwd: password, rol: 0, points: 5, organization: "ULPGC", time_stamp: "17/03/2022 12:13:25"});
+        await testSetupContents().givenAnyUser({name: "nombre2", surname: "apellidos2", display_name: "display2", email: "email2@gmail.com", phone: "123 456 789", passwd: password, rol: 0, points: 5, organization: "ULPGC", time_stamp: "17/03/2022 12:13:25"});
+        let updateData = {
+            id: 1,
+            name: "nombre3",
+            surname: "apellidos3",
+            display_name: "display3",
+            email: "email1@gmail.com",
+            phone: '987654321',
+            passwd: password2,
+            rol: 1,
+            points: 47,
+            organization: "ULL",
+        };
+
+        await appAgent.put("/api/users").send(updateData);
+        let results = await testSetupContents().getAnyUser({name: "nombre3", surname: "apellidos3"});
+        assert.equal(results[0].NAME, "nombre3");
+        assert.equal(results[0].SURNAME, "apellidos3");
+        assert.equal(results[0].DISPLAY_NAME, "display3");
+        assert.equal(results[0].EMAIL, "email1@gmail.com");
+        assert.equal(results[0].PHONE, '987654321');
+        assert.equal(results[0].PASSWD, password2);
+        assert.equal(results[0].ROL, 1);
+        assert.equal(results[0].POINTS, 47);
+        assert.equal(results[0].ORGANIZATION, "ULL");
+    });
 });
