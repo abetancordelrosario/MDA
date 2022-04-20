5793072a6ae1ad14895068ff8ce0d7c924136b2b
Otoniel <otonielcuentas@gmail.com>
2022-04-20 12:54:55 +0100

    Red: Controllers / Messages / API Should delete a response for a specified Message

--- test/controllers/tests.messagesApi.js
+++ test/controllers/tests.messagesApi.js
@@ -87,4 +87,30 @@ describe ("Controllers / Messages / API", function()
         assert.equal(responses[0].id, 1);
         assert.equal(responses[0].text, "Respuesta de Prueba");
     })
+
+    it("Should delete a response for a specified Message", async() =>
+    {
+        const anyResponseToDelete = {
+            messageid: 1,
+            id: 1
+        };
+        const anyResponses = [
+            {
+                id: 1,
+                text: "Respuesta de Prueba"
+            },
+            {
+                id: 2,
+                text: "Respuesta de Prueba 2"
+            }
+        ]
+        await testSetupContents().givenAnyMessage({subjectid: 1, userid: 1, message: "Mensaje de Prueba", responses: JSON.stringify(anyResponses)});
+
+        await appAgent.delete("/api/messages/response").send(anyResponseToDelete);
+
+        results = await testSetupContents().getAnyMessages({subjectid: 1});
+        let responses = JSON.parse(results[0].RESPONSES);
+        assert.equal(responses[0].id, 2);
+        assert.equal(responses[0].text, "Respuesta de Prueba 2");
+    });
 });
