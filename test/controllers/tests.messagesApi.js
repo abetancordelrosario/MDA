ae741b96be0de8eae0f52795fe77724f3ea3358b
Otoniel <otonielcuentas@gmail.com>
2022-04-20 12:42:46 +0100

    Green: Controllers / Messages / API Should return all messages for a specified subject

--- test/controllers/tests.messagesApi.js
+++ test/controllers/tests.messagesApi.js
@@ -28,4 +28,19 @@ describe ("Controllers / Messages / API", function()
         
         assert.equal(results[0].MESSAGE, "Mensaje de Prueba");
     });
+
+    it("Should return all messages for a specified subject", async() =>
+    {
+        await testSetupContents().givenAnyMessage({subjectid: 1, userid: 1, message: "Mensaje de Prueba"});
+        await testSetupContents().givenAnyMessage({subjectid: 1, userid: 2, message: "Mensaje de Prueba 2"});
+        await testSetupContents().givenAnyMessage({subjectid: 2, userid: 3, message: "Mensaje de Prueba 3"});
+        let anyUserSubjectId = 1
+        
+        const response = await appAgent.get("/api/messages/?subjectid=1");
+        let results = JSON.parse(response.text);
+
+        assert.equal(results.length, 2);
+        assert.equal(results[0].MESSAGE, "Mensaje de Prueba");
+        assert.equal(results[1].MESSAGE, "Mensaje de Prueba 2");
+    });
 });
