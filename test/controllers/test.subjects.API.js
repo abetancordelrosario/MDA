4dd40fc9d45fe1a83d928da099ed864219e12ec8
Ione <ione.sanabria101@alu.ulpgc.es>
2022-04-22 18:43:07 +0100

    Red: Controllers / Subjects / API Should update a Subject

--- test/controllers/test.subjects.API.js
+++ test/controllers/test.subjects.API.js
@@ -82,4 +82,30 @@ describe ("Controllers / Subjects / API", function()
         assert.equal(results[1].FACULTY, "facultad2");
         assert.equal(results.length, 2);
     });
+
+    it("Should update a Subject", async() =>
+    {
+        const subjectInfo3 = {
+            id: 1,
+            name: "asignatura3" ,
+            university: "universidad3",
+            faculty: "facultad3"
+        }
+
+        await testSetupContents().givenAnySubject({name: "asignatura1", university: "universidad1", faculty: "facultad1"});
+        await testSetupContents().givenAnySubject({name: "asignatura2", university: "universidad2", faculty: "facultad2"});
+
+        await appAgent.put("/api/subjects").send(subjectInfo3);
+
+        let results = await testSetupContents().getAnySubjects();
+        assert.equal(results.length, 2);
+
+        assert.equal(results[0].NAME, "asignatura3");
+        assert.equal(results[0].UNIVERSITY, "universidad3");
+        assert.equal(results[0].FACULTY, "facultad3");
+        
+        assert.equal(results[1].NAME, "asignatura2");
+        assert.equal(results[1].UNIVERSITY, "universidad2");
+        assert.equal(results[1].FACULTY, "facultad2");
+    });
 });
