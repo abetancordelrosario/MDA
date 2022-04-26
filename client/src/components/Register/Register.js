dd1ae0f2376c1d0fc71707404fa2c126c6a6791b
Otoniel <otonielcuentas@gmail.com>
2022-04-26 09:31:18 +0100

    FIX: Register redirect

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -2,15 +2,18 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import React from 'react';
 import './Register.css';
-import {createUser} from '../../services/userService'
+import {Link, useNavigate} from 'react-router-dom';
+import {createUser} from '../../services/userService';
 let md5 = require("md5")
 
 
-class Register extends React.Component {
+function Register() {
+    
+    const navigate = useNavigate();
 
-    handleSubmit = (event) => {
+    function handleSubmit (event) {
         event.preventDefault()
-        
+
         let userInfo = {
             name: event.target.elements.nombre.value,
             surname: event.target.elements.apellidos.value,
@@ -23,61 +26,60 @@ class Register extends React.Component {
             organization: event.target.elements.universidad.value,
             time_stamp: "17/03/2022 12:12:25"
         }
-        createUser(userInfo)
+        createUser(userInfo);
+        navigate(`/`);
     }
 
-    render() {
-        return (
-            <div>
-            <Header />
-            <div className="register-page">
-                <form onSubmit={this.handleSubmit}>
-                    <div className="register-form">
-                        <h2>¡Regístrate!</h2>
+    return (
+        <div>
+        <Header />
+        <div className="register-page">
+            <form onSubmit={handleSubmit}>
+                <div className="register-form">
+                    <h2>¡Regístrate!</h2>
+                    <div class="col">
+                        <label>Usuario</label>
+                        <input type="text" class="form-control" name="usuario" required></input>
+                    </div>
+                    <div class="row">
                         <div class="col">
-                            <label>Usuario</label>
-                            <input type="text" class="form-control" name="usuario"></input>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <label>Nombre</label>
-                                <input type="text" class="form-control" name="nombre"/>
-                            </div>
-                            <div class="col">
-                                <label>Apellidos</label>
-                                <input type="text" class="form-control" name="apellidos"/>
-                            </div>
+                            <label>Nombre</label>
+                            <input type="text" class="form-control" name="nombre" required/>
                         </div>
                         <div class="col">
-                            <label>Correo Electrónico</label>
-                            <input type="email" class="form-control" name="correo"/>
-                        </div>
-                        <div class="col">
-                            <label>Teléfono</label>
-                            <input type="text" class="form-control" name="telefono"/>
-                        </div>
-                        <div class="col">
-                            <label>Universidad</label>
-                            <input type="text" class="form-control" name="universidad"/>
-                        </div>
-                        <div class="col">
-                            <label>Contraseña</label>
-                            <input type="passwd" class="form-control" name="contra"/>
-                        </div>
-                        <div class="col">
-                            <label>Repite tu contraseña</label>
-                            <input type="passwd" class="form-control" name="repcontra"/>
+                            <label>Apellidos</label>
+                            <input type="text" class="form-control" name="apellidos" required/>
                         </div>
                     </div>
-                    <div className="login-button">
-                        <button type="submit">Regístrate</button>
+                    <div class="col">
+                        <label>Correo Electrónico</label>
+                        <input type="email" class="form-control" name="correo" required/>
                     </div>
-                </form>
-            </div>
-            <Footer />
-            </div>
-        )
-    }
+                    <div class="col">
+                        <label>Teléfono</label>
+                        <input type="text" class="form-control" name="telefono" required/>
+                    </div>
+                    <div class="col">
+                        <label>Universidad</label>
+                        <input type="text" class="form-control" name="universidad" required/>
+                    </div>
+                    <div class="col">
+                        <label>Contraseña</label>
+                        <input type="password" class="form-control" name="contra" required/>
+                    </div>
+                    <div class="col">
+                        <label>Repite tu contraseña</label>
+                        <input type="password" class="form-control" name="repcontra" required/>
+                    </div>
+                </div>
+                <div className="login-button">
+                    <button type="submit">Regístrate</button>
+                </div>
+            </form>
+        </div>
+        <Footer />
+        </div>
+    )
 
 }
 export default Register;
