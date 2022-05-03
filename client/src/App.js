c205ff5ab855f77275a6b19916838b81795a6094
Carlos Santana Rodríguez <carlos.santana.pelusa@gmail.com>
2022-05-03 12:51:50 +0100

    Front Edited

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <ListaRespuestas />
+      <Home />
       <Footer />
     </div>
   );
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -4,7 +4,15 @@ import './Home.css'
 function Home () {
     return (
         <div className="home-page">
-            <input type="text" className="form-control-lg col-sm-8" name="buscador" placeholder='Universidad, facultad, asignatura...'></input>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+            <div class="search">
+                <div class="search-container">
+                    <h4>Haz tu búsqueda</h4>
+                    <input type="text" className="form-control-lg col-sm-8" name="buscador" placeholder='Universidad, facultad, asignatura...'></input>
+                </div>
+            </div>
         </div>
     )
 }
